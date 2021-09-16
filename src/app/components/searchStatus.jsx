c044e2407165091e9fdf707f2041dab8c4eeb19a
NikePro777 <evgenij.datskij@mail.ru>
2021-09-16 23:23:02 +0500

    finish search status

--- src/app/components/searchStatus.jsx
+++ src/app/components/searchStatus.jsx
@@ -1,18 +1,13 @@
 import React from "react";
 
-const SearchStatus = () =>
-  // { length, renderPhrase, renderColor }
-  {
-    return (
-      <>
-        <h1
-        // style={{ fontSize: 20 }} className={renderColor(length)}
-        >
-          {/* {renderPhrase(length)} */}
-          dfsfsf
-        </h1>
-      </>
-    );
-  };
+const SearchStatus = ({ length, renderPhrase, renderColor }) => {
+  return (
+    <>
+      <h1 style={{ fontSize: 20 }} className={renderColor(length)}>
+        {renderPhrase(length)}
+      </h1>
+    </>
+  );
+};
 
 export default SearchStatus;
