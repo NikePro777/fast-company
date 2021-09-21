2ee0d1137444fc0b742ed319e0a4bb24308a0ce2
NikePro777 <evgenij.datskij@mail.ru>
2021-09-21 12:27:54 +0500

    fixed search status

--- src/App.js
+++ src/App.js
@@ -7,46 +7,12 @@ const App = () => {
   const [users, setUsers] = useState(api.users.fetchAll());
 
   const handleDelete = (userId) => {
-    let timed = users.filter((item) => {
-      return item._id !== userId.id;
-    });
-    setUsers(timed);
+    setUsers(
+      users.filter((item) => {
+        return item._id !== userId.id;
+      })
+    );
   };
-
-  const renderPhrase = (length) => {
-    let phrase = " с тобой сегодня";
-    if (
-      length < 10 &&
-      (length % 10 === 2 || length % 10 === 3 || length % 10 === 4)
-    ) {
-      phrase = " человека тусанут" + phrase;
-    } else {
-      phrase = " человек тусанет" + phrase;
-    }
-    if (length === 0) {
-      phrase = "Никто с тобой не тусанет";
-      return phrase;
-    }
-    return length + phrase;
-  };
-
-  const renderColor = (number) => {
-    let color = "m-2 badge bg-";
-    if (number === 0) {
-      color = color + "danger";
-    } else {
-      color = color + "primary";
-    }
-    return color;
-  };
-
-  // acc = timing.findIndex((item) => {
-  //   let value = Object.values(id);
-  //   return item._id === value[0];
-  // });
-  // timing[acc].status = !timing[acc].status;
-  // console.log(timing[acc].status);
-  // return timing[acc].status;
 
   const handleToggleBookMark = (id) => {
     setUsers(
@@ -61,11 +27,7 @@ const App = () => {
 
   return (
     <>
-      <SearchStatus
-        length={users.length}
-        renderPhrase={renderPhrase}
-        renderColor={renderColor}
-      />
+      <SearchStatus length={users.length} />
       <Users
         users={users}
         handleDelete={handleDelete}
