ad18978f14d97b2d0bb1c93c5b944f2abab1a70e
evgeniy.datskiy <evgenij.datskij@mail.ru>
2022-02-15 19:38:13 +0500

    fix

--- src/app/App.js
+++ src/app/App.js
@@ -4,15 +4,15 @@ import Login from "./layouts/login";
 import Main from "./layouts/main";
 import NavBar from "./components/ui/navBar";
 import Quation from "./layouts/users";
-import UserEdit from "./components/page/userEdit";
+// import UserEdit from "./components/page/userEdit";
 
 const App = () => {
   return (
     <div>
       <NavBar />
       <Switch>
-        <Route path="/users/:userId?/edit" component={UserEdit} />
-        <Route path="/users/:userId?" component={Quation} />
+        {/* <Route path="/users/userId/edit" component={UserEdit} /> */}
+        <Route path="/users/:userId?/:edit?" component={Quation} />
         <Route path="/login/:type?" component={Login} />
         <Route path="/" exact component={Main} />
         <Redirect to="/" />
--- src/app/components/page/userPage/userPage.jsx
+++ src/app/components/page/userPage/userPage.jsx
@@ -1,26 +1,22 @@
 import React, { useState, useEffect } from "react";
 import api from "../../../api";
-import { useHistory, Link } from "react-router-dom";
+// import { useHistory } from "react-router-dom";
 import PropTypes from "prop-types";
 import Qualities from "../../ui/qualities";
-
+import UserEdit from "../userEdit";
 const UserPage = ({ userId }) => {
-  const history = useHistory();
+  // const history = useHistory();
   const [user, setUser] = useState();
   const handleClick = () => {
-    const { userId } = history;
     // history.push("/users/edit");
     // <UserPage userId={userId} />
-    return (
-      <Link
-        className="nav-link "
-        userID={userId}
-        aria-current="page"
-        to="/users/:userId/edit"
-      >
-        Users
-      </Link>
-    );
+    const id = userId;
+    console.log(id);
+    if (id) {
+      return <UserEdit userId={id} />;
+    } else {
+      console.log(`no`);
+    }
   };
   useEffect(() => {
     api.users.getById(userId).then((data) => setUser(data));
