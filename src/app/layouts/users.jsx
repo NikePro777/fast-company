1a6768a974d7ff883d0f75e7c53a7e1b0fecbcc1
evgeniy.datskiy <evgenij.datskij@mail.ru>
2022-03-19 20:29:12 +0500

    finish window edit

--- src/app/layouts/users.jsx
+++ src/app/layouts/users.jsx
@@ -2,12 +2,25 @@ import React from "react";
 import UsersListPage from "../components/page/usersListPage";
 import UserPage from "../components/page/userPage";
 import { useParams } from "react-router-dom";
+import EditUserPage from "../components/page/editUserPage/editUserPage";
 
 const Users = () => {
   const params = useParams();
+  const { userId, edit } = params;
 
-  const { userId } = params;
-  return <>{userId ? <UserPage userId={userId} /> : <UsersListPage />}</>;
+  return (
+    <>
+      {userId ? (
+        edit ? (
+          <EditUserPage />
+        ) : (
+          <UserPage userId={userId} />
+        )
+      ) : (
+        <UsersListPage />
+      )}
+    </>
+  );
 };
 
 export default Users;
